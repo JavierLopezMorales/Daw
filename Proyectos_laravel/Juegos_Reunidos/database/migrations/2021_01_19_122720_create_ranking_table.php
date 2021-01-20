be587f4ff308ec9434058893d6bf2d4740ff585d
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-01-20 19:22:18 +0100

    Add files via upload

---
+++ Proyectos_laravel/Juegos_Reunidos/database/migrations/2021_01_19_122720_create_ranking_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRankingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ranking-slide', function (Blueprint $table) {
+            $table->id();
+            $table->integer('score');
+            $table->date('date');
+            $table->string('mode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ranking');
+    }
+}
