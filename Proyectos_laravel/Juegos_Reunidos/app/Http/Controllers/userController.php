09554794edb2f91ee3bd649953b51ed4b8ca52d9
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-01-27 18:52:48 +0100

    Add files via upload

---
+++ Proyectos_laravel/Juegos_Reunidos/app/Http/Controllers/userController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\usuarios;
+
+class UserController extends Controller
+{
+    //
+    public function index() {
+        $userList = usuarios::all();
+        return view('showUsuarios', ['userList'=>$userList]);
+    }
+
+
+    //
+    // public function create() {
+    //     return view('usuarios.form');
+    // }
+    //
+    // public function store(Request $r) {
+    //     $usuario = new User();
+    //     $usuario->email = $r->email;
+    //     $usuario->name = $r->name;
+    //     $usuario->password = $r->password;
+    //     $usuario->save();
+    //     return redirect()->route('user.index');
+    // }
+    //
+    // public function edit($id) {
+    //     $usuario = User::find($id);
+    //     return view('usuarios.form', array('user' => $user));
+    // }
+    //
+    // public function update(Request $r) {
+    //     $usuario = User::find($r->id);
+    //     $usuario->email = $r->email;
+    //     $usuario->name = $r->name;
+    //     $usuario->password = $r->password;
+    //     $usuario->save();
+    //     return redirect()->route('user.index');
+    // }
+    //
+    // public function destroy($id) {
+    //     $usuarior = User::find($id);
+    //     $usuario->delete();
+    //     return redirect()->route('user.index');
+    // }
+}
