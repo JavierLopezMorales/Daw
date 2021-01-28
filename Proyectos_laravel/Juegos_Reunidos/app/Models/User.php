d9dcf9ef67eb9650ce336170be84dfedcca038fd
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-01-28 13:20:00 +0100

    Update User.php

--- Proyectos_laravel/Juegos_Reunidos/app/Models/User.php
+++ Proyectos_laravel/Juegos_Reunidos/app/Models/User.php
@@ -10,6 +10,8 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    protected $table = 'usuarios'; // Nombre de la tabla
+      protected $primaryKey = 'id'; // Nombre de la clave primaria
 
     /**
      * The attributes that are mass assignable.
