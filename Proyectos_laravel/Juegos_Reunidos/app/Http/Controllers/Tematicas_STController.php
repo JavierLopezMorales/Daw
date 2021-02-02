f0dd82852d1fc39f6598ac4215176592bfff975f
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2021-02-02 13:40:31 +0100

    Add files via upload

---
+++ Proyectos_laravel/Juegos_Reunidos/app/Http/Controllers/Tematicas_STController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Tematicas_STController extends Controller
+{
+  public function index() {
+       $tematicasList = TematicasST::all();
+       return view('showTematicasST', ['tematicasList'=>$tematicasList]);
+   }
+
+
+
+   public function create() {
+       return view('formTematicasST');
+   }
+
+   public function store(Request $r) {
+       $tematica = new TematicasST();
+       $tematica->type = $r->type;
+       $tematica->name = $r->name;
+       $tematica->description = $r->description;
+       $tematica->img_fondo = $r->img_fondo;
+       $tematica->img_puzzle = $r->img_puzzle;
+       $tematica->save();
+       return redirect()->route('TematicasST.index');
+   }
+   public function show($id)
+       {
+           //
+       }
+
+   public function edit($id) {
+       $tematicas = TematicasST::find($id);
+       return view('formTematicasST', array('Tematica' => $tematicas));
+   }
+
+   public function update(Request $r,$id) {
+       $tematica = TematicasST::find($r->id);
+       $tematica->type = $r->type;
+       $tematica->name = $r->name;
+       $tematica->description = $r->description;
+       $tematica->img_fondo = $r->img_fondo;
+       $tematica->img_puzzle = $r->img_puzzle;
+       $tematica->save();
+       return redirect()->route('TematicasST.index');
+   }
+
+   public function destroy($id) {
+       $tematica = TematicasST::find($id);
+       $tematica->delete();
+       return redirect()->route('TematicasST.index');
+   }
+}
