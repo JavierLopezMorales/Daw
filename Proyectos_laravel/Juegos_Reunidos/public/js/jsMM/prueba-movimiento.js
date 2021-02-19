bf38c4345969ece57d70c8881f30cc32eea709b0
Javier Lopez Morales <javizapillo2@gmail.com>
2021-02-19 14:13:04 +0100

    Cambios al movimiento del jugador

--- Proyectos_laravel/Juegos_Reunidos/public/js/jsMM/prueba-movimiento.js
+++ Proyectos_laravel/Juegos_Reunidos/public/js/jsMM/prueba-movimiento.js
@@ -1,47 +1,90 @@
-var speed;
+$(document).ready(function(){
 
+    var speed;
+    var posX;
+    var posY;
+
+    // Calcula las coordenadas del player
+    function coordinates(){
+        posX = (100 * parseFloat($('#player').css('left')) / (parseFloat($('.main-container').css('width'))-parseFloat($('#player').css('width'))));
+        posY = (100 * parseFloat($('#player').css('top')) / (parseFloat($('.main-container').css('height'))-parseFloat($('#player').css('height'))));
+        $('.info-left').html(posX + '%');
+        $('.info-top').html(posY + '%');
+    }
+
+    // Velocidad a la que el player se mueve, es mas despacio si se mueve en diagonal
     function calcSpeed(){
         if(kd.LEFT.isDown() == true && kd.UP.isDown() == true || kd.LEFT.isDown() == true && kd.DOWN.isDown() == true || kd.UP.isDown() == true && kd.RIGHT.isDown() == true || kd.RIGHT.isDown() == true && kd.DOWN.isDown() == true){
-            speed = 8;
+            speed = 1;
         }
         else{
-            speed = 10;
+            speed =  1;
         }
     }
 
-    kd.LEFT.down(function () {
+    coordinates();
+    
+    // Mueve el player dependiendo que tecla pulsas
+    kd.LEFT.down(function () { 
+        coordinates();    
         calcSpeed();
-        var x = parseFloat($('#player').css('left'));
-        x -= speed;
-        $('#player').css('left', x + 'px');
+        if(posX <= 0){
+        }
+        else{
+            posX = posX - 2;
+        }
+        $('#player').css('left', posX + '%');
+        
+    });
 
-   });
+    kd.UP.down(function () {
+        coordinates();   
+        calcSpeed();
+        if(posY <= 0){
+        }
+        else{
+            posY -= speed;
+        }
+        $('#player').css('top', posY + '%');
+    });
 
-   kd.UP.down(function () {
-    calcSpeed();
-        var x = parseFloat($('#player').css('top'));
-        x -= speed;
-        $('#player').css('top', x + 'px');
+    kd.RIGHT.down(function () {
+        coordinates();   
+        calcSpeed();
+        if(posX >= 100){
+        }
+        else{
+            posX += speed;
+        }
+        $('#player').css('left', posX + '%');
+    });
 
-   });
+    kd.DOWN.down(function () {
+        coordinates();   
+        calcSpeed();
+        if(posY >= 100){
+        }
+        else{
+            posY += speed;
+        }
+        $('#player').css('top', posY + '%');
+    });
 
-   kd.RIGHT.down(function () {
-    calcSpeed();
-        var x = parseFloat($('#player').css('left'));
-        x += speed;
-        $('#player').css('left', x + 'px');
+   // Funcion para disparar
+    kd.SPACE.down(function () {
 
-   });
+    $('#player').css('background-color', 'red');
 
-   kd.DOWN.down(function () {
-    calcSpeed();
-        var x = parseFloat($('#player').css('top'));
-        x += speed;
-        $('#player').css('top', x + 'px');
+    });
+    kd.SPACE.up(function(){
+        $('#player').css('background-color', 'yellow');
+    });
 
-   });
 
    // This update loop is the heartbeat of Keydrown
-   kd.run(function () {
+    kd.run(function () {
      kd.tick();
-   });
+    });
+
+});
+
